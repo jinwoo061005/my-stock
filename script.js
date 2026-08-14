9a548c661ebc62dc614401b3e0fb0f734aff07ec
jinwoo061005 <seojinwoo061005@gmail.com>
2026-08-14 11:59:11 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -1,19 +1,108 @@
-const holdings=[
-{symbol:'NVDY',shares:14,buy:0},
-{symbol:'QQQM',shares:0.759174,buy:0},
-{symbol:'SCHD',shares:5,buy:0},
-{symbol:'SPMO',shares:1.336567,buy:0},
-{symbol:'SKADR',shares:3,buy:0},
+const API_URL = "/api/quote";
+
+const stocks = [
+  {
+    symbol: "NVDY",
+    shares: Number(localStorage.getItem("NVDY_shares")) || 0,
+    buy: Number(localStorage.getItem("NVDY_buy")) || 0
+  },
+  {
+    symbol: "QQQM",
+    shares: Number(localStorage.getItem("QQQM_shares")) || 0,
+    buy: Number(localStorage.getItem("QQQM_buy")) || 0
+  },
+  {
+    symbol: "SCHD",
+    shares: Number(localStorage.getItem("SCHD_shares")) || 0,
+    buy: Number(localStorage.getItem("SCHD_buy")) || 0
+  },
+  {
+    symbol: "SPMO",
+    shares: Number(localStorage.getItem("SPMO_shares")) || 0,
+    buy: Number(localStorage.getItem("SPMO_buy")) || 0
+  },
+{
+  symbol: "VIG",
+  shares: Number(localStorage.getItem("VIG_shares")) || 0,
+  buy: Number(localStorage.getItem("VIG_buy")) || 0
+},
+  {
+    symbol: "SKADR",
+    shares: Number(localStorage.getItem("SKADR_shares")) || 0,
+    buy: Number(localStorage.getItem("SKADR_buy")) || 0
+  }
 ];
-const box=document.getElementById('stocks');
-holdings.forEach(h=>{
- const d=document.createElement('div');
- d.className='stock';
- d.innerHTML=`<div class="row"><b>${h.symbol}</b><span id="${h.symbol}-price">--</span></div>
- 보유 <input type="number" step="0.000001" value="${h.shares}"
- onchange="localStorage.setItem('shares_${h.symbol}',this.value)">
- 매수가 <input type="number" step="0.01" value="${h.buy}"
- onchange="localStorage.setItem('buy_${h.symbol}',this.value)">
- <div id="${h.symbol}-change">--</div>`;
- box.appendChild(d);
+
+let usdKrw = 0;
+
+function saveValue(symbol) {
+
+  const shares =
+    Number(
+      document.getElementById(symbol + "-shares").value
+    ) || 0;
+
+  const buy =
+    Number(
+      document.getElementById(symbol + "-buy").value
+    ) || 0;
+
+  localStorage.setItem(
+    symbol + "_shares",
+    shares
+  );
+
+  localStorage.setItem(
+    symbol + "_buy",
+    buy
+  );
+
+  const stock =
+    stocks.find(
+      s => s.symbol === symbol
+    );
+
+  stock.shares = shares;
+  stock.buy = buy;
+
+  updateTotal();
+}
+
+stocks.forEach(stock => {
+
+  window.addEventListener(
+    "load",
+    () => {
+
+      const s =
+        document.getElementById(
+          stock.symbol + "-shares"
+        );
+
+      const b =
+        document.getElementById(
+          stock.symbol + "-buy"
+        );
+
+      if (s) {
+
+        s.value = stock.shares;
+
+        s.onchange = () =>
+          saveValue(stock.symbol);
+
+      }
+
+      if (b) {
+
+        b.value = stock.buy;
+
+        b.onchange = () =>
+          saveValue(stock.symbol);
+
+      }
+
+    }
+  );
+
 });
