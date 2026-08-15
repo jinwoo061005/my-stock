d572be37a5c4d909a01045888cf6b04032d79c05
jinwoo061005 <seojinwoo061005@gmail.com>
2026-08-15 11:25:12 +0900

    api.js 업데이트

--- api.js
+++ api.js
@@ -1,13 +1,27 @@
+// api.js
+
 const API_URL = "/api/quote";
 
 export async function loadQuotes() {
-  const response = await fetch(API_URL, {
-    cache: "no-store"
-  });
 
-  if (!response.ok) {
-    throw new Error(`HTTP ${response.status}`);
-  }
+    try {
 
-  return await response.json();
+        const response = await fetch(API_URL, {
+            cache: "no-store"
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP ${response.status}`);
+        }
+
+        return await response.json();
+
+    } catch (error) {
+
+        console.error("API 오류 :", error);
+
+        return null;
+
+    }
+
 }
