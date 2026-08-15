e70a604daf7ff5f741fbfb9571d9d1246834537c
jinwoo061005 <seojinwoo061005@gmail.com>
2026-08-15 11:18:07 +0900

    main.js 업데이트

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-main.js
+import { loadQuotes } from "./api.js";
+
+async function refreshQuotes() {
+
+  try {
+
+    const data = await loadQuotes();
+
+    updateMarketData(data);
+
+  } catch (e) {
+
+    console.error(e);
+
+  }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  setupEvents();
+
+  refreshQuotes();
+
+  setInterval(refreshQuotes,60000);
+
+});
